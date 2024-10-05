ae05f86e8c672ed73241e252439b1d6a64e3c983
PedroDantasLeite <pedantasleite@gmail.com>
2024-10-05 10:19:08 -0300

    Header + Action bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,25 @@ import './App.css'
 function App() {
 
   return (
-    <div>portfolio</div>
+    <div className='header'>
+      <h1 className='headerTitle'>Pedro Dantas Leite</h1>
+      <div className='headerSubtitle'>Desenvolvedor Full Stack</div>
+      <span className='actionBar'>
+        <button title="Resume">
+          <img className='invert' src='/icons8-resume-100.png'/>
+        </button>
+        <button title="Linkedin">
+          <img src='/linkedin-icon-2.svg'/>
+        </button>
+        <button title="Github">
+          <img src='/github-icon-2.svg'/>
+        </button>
+        <button title="Leetcode ">
+          <img className='invert' src='/leetcode-svgrepo-com.svg'/>
+        </button>
+      </span>
+    </div>
+
   )
 }
 
