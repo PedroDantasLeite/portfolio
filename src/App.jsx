ca5854852cb77f713edcdef360407e3b17853799
PedroDantasLeite <pedantasleite@gmail.com>
2024-10-05 10:20:43 -0300

    Prettier formating

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='header'>
-      <h1 className='headerTitle'>Pedro Dantas Leite</h1>
-      <div className='headerSubtitle'>Desenvolvedor Full Stack</div>
-      <span className='actionBar'>
-        <button title="Resume">
-          <img className='invert' src='/icons8-resume-100.png'/>
-        </button>
-        <button title="Linkedin">
-          <img src='/linkedin-icon-2.svg'/>
-        </button>
-        <button title="Github">
-          <img src='/github-icon-2.svg'/>
-        </button>
-        <button title="Leetcode ">
-          <img className='invert' src='/leetcode-svgrepo-com.svg'/>
-        </button>
-      </span>
-    </div>
-
-  )
+    <>
+      <div className="header">
+        <h1 className="headerTitle">Pedro Dantas Leite</h1>
+        <div className="headerSubtitle">Desenvolvedor Full Stack</div>
+        <span className="actionBar">
+          <button title="Resume">
+            <img className="invert" src="/icons8-resume-100.png" />
+          </button>
+          <button title="Linkedin">
+            <img src="/linkedin-icon-2.svg" />
+          </button>
+          <button title="Github">
+            <img src="/github-icon-2.svg" />
+          </button>
+          <button title="Leetcode ">
+            <img className="invert" src="/leetcode-svgrepo-com.svg" />
+          </button>
+        </span>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
